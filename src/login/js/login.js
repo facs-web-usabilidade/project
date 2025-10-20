321e0af070325a24ea6670367718385da800d7f8
Marcelo <19808040+mmcav@users.noreply.github.com>
2025-10-20 18:26:55 -0300

    feat(login): wip página de login

---
+++ src/login/js/login.js
@@ -0,0 +1,12 @@
+document.getElementById("login-button").addEventListener("submit", async function (e) {
+    e.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value;
+
+    try {
+        throw new Error("wipwip");
+    } catch (error) {
+        console.log("wip");
+    }
+});

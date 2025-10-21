7aee39b5adf9a7a082bbccd41db53ca7759a9eb0
julio-git <you@example.com>
2025-10-20 23:09:39 -0300

    fix(home): correção bug do card carrossel

--- src/home/js/slider.js
+++ src/home/js/slider.js
@@ -1,4 +1,4 @@
-const titles = [
+const bestsellers = [
     "Hollow Knight: Silksong",
     "Death Stranding",
     "Split Fiction",
@@ -6,20 +6,45 @@ const titles = [
     "Pokémon Emerald"
 ];
 
-let index = 0;
-const card = document.getElementById("bestseller-card");
+const toprated = [
+    "Stardew Valley",
+    "Death Stranding",
+    "Split Fiction",
+    "Hollow Knight: Silksong",
+    "Pokémon Emerald"
+];
 
-function showTitle(i) {
-    index = (i + titles.length) % titles.length;
+let index1 = 0,  index2 = 0;
+const card_bestsellers = document.getElementById("card-slider-bestsellers");
+const card_toprated = document.getElementById("card-slider-toprated");
+
+function showTitle1(i) {
+    index1 = (i + bestsellers.length) % bestsellers.length;
     setTimeout(() => {
-        card.textContent = titles[index];
+        card_bestsellers.textContent = bestsellers[index1];
     }, 200); // delay para mudar o content do card, apenas para parecer mais suave - CSS também tem transition ease
 };
 
-function prevTitle() { 
-    showTitle(index - 1);
+function showTitle2(i) {
+    index2 = (i + toprated.length) % toprated.length;
+    setTimeout(() => {
+        card_toprated.textContent = toprated[index2];
+    }, 200); // delay para mudar o content do card, apenas para parecer mais suave - CSS também tem transition ease
 };
 
-function nextTitle() {
-    showTitle(index + 1);
+function prevTitle1() { 
+    showTitle1(index1 - 1);
+};
+
+function nextTitle1() {
+    showTitle1(index1 + 1);
+};
+
+
+function prevTitle2() { 
+    showTitle2(index2 - 1);
+};
+
+function nextTitle2() {
+    showTitle2(index2 + 1);
 };

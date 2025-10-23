af781b3096be920e8502670819464438a1a39c3c
julio-git <you@example.com>
2025-10-23 20:11:05 -0300

    feat: dropdown menu do perfil

--- src/profile/js/sidebar.js
+++ src/profile/js/sidebar.js
@@ -4,3 +4,11 @@ const hamburger = document.getElementById("hamburger_menu_icon");
 hamburger.addEventListener("click", () => {
     sidebar.classList.toggle("hidden");
 });
+
+// // Pode ser interessante:
+// // Fechar a sidebar ao clicar fora dos links
+// document.addEventListener("click", (e) => {
+//     if (!hamburger.contains(e.target)) {
+//         sidebar.classList.toggle("hidden");
+//     }
+// });

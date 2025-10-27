ec6fe3f2b3a6ccaf58b9e7bd5abc9dc7a77ab1c4
Marcelo <19808040+mmcav@users.noreply.github.com>
2025-10-26 22:18:25 -0300

    feat(login): wip lógica da página de login

--- src/login/js/login.js
+++ src/login/js/login.js
@@ -1,12 +1,81 @@
-document.getElementById("login-button").addEventListener("submit", async function (e) {
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("senha");
+const emailError = document.getElementById("email-error");
+const passwordError = document.getElementById("password-error");
+const loginStatus = document.getElementById("login-status");
+const loginButton = document.getElementById("login-button");
+const form = document.getElementById("login-form");
+
+emailInput.addEventListener("input", () => emailError.textContent = "");
+
+passwordInput.addEventListener("input", () => passwordError.textContent = "");
+
+function validateInput(input, errorTextField, errorMessage) {
+    if (!input) {
+        errorTextField.textContent = errorMessage;
+        return false;
+    }
+    return true;
+}
+
+form.addEventListener("submit", async function (e) {
     e.preventDefault();
 
-    const email = document.getElementById("email").value;
-    const senha = document.getElementById("senha").value;
+    loginStatus.textContent = "";
+    loginStatus.style.color = "white";
+    emailError.textContent = "";
+    passwordError.textContent = "";
+
+    const email = emailInput.value.trim();
+    const validEmail = validateInput(email, emailError, "Por favor, digite o seu email no campo acima.");
+
+    const password = passwordInput.value.trim();
+    const validPassword = validateInput(password, passwordError, "Por favor, digite a sua senha no campo acima.");
+
+    if (!validEmail || !validPassword) {
+        return;
+    }
+
+    const usuario = {
+        "email": email,
+        "senha": password
+    };
 
     try {
-        throw new Error("wipwip");
+        loginButton.disabled = true;
+        loginStatus.textContent = "Verificando suas credenciais, por favor aguarde...";
+
+        const response = await fetch('http://localhost:3000/api/v1/auth/login', {
+          method: "POST",
+          body: JSON.stringify(usuario),
+          headers: {
+            "Content-type": "application/json",
+          }
+        });
+
+        const data = await response.json();
+
+        await sleep(5000); // função para simular artificialmente a resposta demorada da api
+
+        if (response.ok) {
+            loginStatus.textContent = "Credenciais verificadas com sucesso! Em breve você será redirecionado(a) para a loja, por favor aguarde...";
+            loginStatus.style.color = "green";
+            alert(data.token);
+            await sleep(2000); // função para simular artificialmente a resposta demorada da api
+            window.location.href = "../home/home.html";
+        } else {
+            loginStatus.textContent = "Email ou senha incorretos.";
+            loginStatus.style.color = "red";
+        }
     } catch (error) {
-        console.log("wip");
+        loginStatus.textContent = "Não foi possível contactar o servidor. Tente novamente mais tarde.";
+        loginStatus.style.color = "red";
+    } finally {
+        loginButton.disabled = false;
     }
 });
+
+// função para simular artificialmente a resposta demorada da api
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}

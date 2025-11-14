511746d639492b1d890a26c8c4c6793e44f19338
Marcelo <19808040+mmcav@users.noreply.github.com>
2025-11-13 21:28:25 -0300

    fix: tentativa de correção de caixa alta do nome do arquivo

---
+++ react/supa-games/src/pages/MyGames1.js
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+import "../styles/pages/myGames.css";
+
+const MyGames = () => {
+  // useEffect(() => {
+
+  // }, []);
+
+  return (
+    <main>
+
+    </main>
+  );
+};
+
+export default MyGames;

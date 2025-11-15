d1bb8037910a30fd367fe8f9cca89f9472866769
julio-git <you@example.com>
2025-11-15 02:51:50 -0300

    add: path para Games e gameInfo

--- react/supa-games/src/App.js
+++ react/supa-games/src/App.js
@@ -4,6 +4,7 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Games from "./pages/Games";
+import GameInfo from "./pages/GameInfo";
 import Genres from "./pages/Genres";
 import Wishlist from "./pages/Wishlist";
 import Profile from "./pages/Profile";
@@ -17,6 +18,8 @@ const App = () => (
       {/* <Route element={<Layout />}> */}
       <Route path="/home" element={<Home />} />
       <Route path="/games" element={<Games />} />
+      <Route path="/games/gameInfo" element={<GameInfo />} />
+      <Route path="games/gameInfo/:id" element={<GameInfo />} />
       <Route path="/genres" element={<Genres />} />
       <Route path="/wishlist" element={<Wishlist />} />
       <Route path="/profile" element={<Profile />} />

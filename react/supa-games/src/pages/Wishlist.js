4dade46137a58367337b8ec4753ffa57df3912e5
ZanoniCFernandes <zanoni.c.fernandes@gmail.com>
2025-11-18 23:40:28 -0300

    feat: first commit wishlist page

--- react/supa-games/src/pages/Wishlist.js
+++ react/supa-games/src/pages/Wishlist.js
@@ -1,16 +1,24 @@
 import { useEffect } from "react";
 import "../styles/pages/wishlist.css";
+import Layout from "../components/Layout";
 
-const Wishlist = () => {
-    useEffect(() => {
-        
-    }, []);
+function Wishlist() {
+  useEffect(() => {
 
-    return (
-        <main>
-            
-        </main> 
-    );
+  }, []);
+
+  return (
+    <Layout>
+      <main className="content">
+        <aside>
+          <h1>left container</h1>
+        </aside>
+        <aside>
+          <h1>right container</h1>
+        </aside>
+      </main>
+    </Layout>
+  );
 };
 
 export default Wishlist;

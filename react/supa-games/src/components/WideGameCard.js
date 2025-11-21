284a6c4ec408641b81712c8f5448051d29092ab9
ZanoniCFernandes <zanoni.c.fernandes@gmail.com>
2025-11-21 20:31:44 -0300

    chore: adjust component nomenclature

---
+++ react/supa-games/src/components/WideGameCard.js
@@ -0,0 +1,14 @@
+function WideGameCard({ id, game, imgSrc, altTxt }) {
+  return (
+    <div>
+      <a href={game ? `/games/${game.id}` : `#`}>
+        <div className="carde" id={id}>
+          <img src={imgSrc} alt={altTxt} />
+          <p id="card-slider-toprated">{game.nome}</p>
+        </div>
+      </a>
+    </div >
+  );
+}
+
+export default WideGameCard;

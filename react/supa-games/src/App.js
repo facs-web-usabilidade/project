dea2bccf403d4d4eed2525b639ddb602297fd6fb
Marcelo <19808040+mmcav@users.noreply.github.com>
2025-11-22 15:41:28 -0300

    feat: autorização admin

--- react/supa-games/src/App.js
+++ react/supa-games/src/App.js
@@ -12,6 +12,7 @@ import History from "./pages/History";
 import Settings from "./pages/Settings";
 import VerifyToken from "./components/VerifyToken";
 import VerifyNoToken from "./components/VerifyNoToken";
+import VerifyAdmin from "./components/VerifyAdmin";
 
 const App = () => (
   <Router>
@@ -31,6 +32,9 @@ const App = () => (
           <Route path="/myGames" element={<MyGames />}/>
           <Route path="/history" element={<History />}/>
           <Route path="/settings" element={<Settings />}/>
+          <Route element={<VerifyAdmin />}>
+            {/* admin only routes */}
+          </Route>
         </Route>
       </Route>
       <Route path="*" element={<Navigate to="/" replace />} />

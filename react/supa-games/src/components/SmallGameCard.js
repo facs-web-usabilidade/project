8e092583b3ac80caf845a96b55acf1fddd2083f3
ZanoniCFernandes <zanoni.c.fernandes@gmail.com>
2025-11-22 15:58:55 -0300

    fix: small e wide game card properties fix

--- react/supa-games/src/components/SmallGameCard.js
+++ react/supa-games/src/components/SmallGameCard.js
@@ -1,9 +1,9 @@
 function SmallGameCard({ classId, game, imgSrc, altTxt }) {
   return (
     <a className="card-link" href={game.id ? `/games/${game.id}` : `#`}>
-      <div className="card" id={classId}>
+      <div className="card" id={classId ? classId : 'default'}>
         <img src={game.img ? game.img : imgSrc} alt={game.altTxt ? game.altTxt : altTxt} />
-        <p className="game-title-img">{game.nome}</p>
+        <p className="game-title-img">{game.nome? game.nome : 'no data'}</p>
       </div>
     </a>
   );
--- react/supa-games/src/components/WideGameCard.js
+++ react/supa-games/src/components/WideGameCard.js
@@ -1,10 +1,10 @@
-function WideGameCard({ id, game, imgSrc, altTxt }) {
+function WideGameCard({ classId, game, imgSrc, altTxt }) {
   return (
     <div>
-      <a href={game ? `/games/${game.id}` : `#`}>
-        <div className="carde" id={id}>
-          <img src={imgSrc} alt={altTxt} />
-          <p id="card-slider-toprated">{game.nome}</p>
+      <a href={game.id ? `/games/${game.id}` : `#`}>
+        <div className="carde" id={classId? classId : 'default'}>
+          <img src={game.img ? game.img : imgSrc} alt={game.altTxt ? game.altTxt : altTxt} />
+          <p id="card-slider-toprated">{game.nome? game.nome : 'no data'}</p>
         </div>
       </a>
     </div >

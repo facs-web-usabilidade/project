d319002e1a9d0799a33699ed72138f2126223111
julio-git <you@example.com>
2025-11-23 01:26:59 -0300

    feat: add path dinâmico no SmallGameCard

--- react/supa-games/src/components/SmallGameCard.js
+++ react/supa-games/src/components/SmallGameCard.js
@@ -1,6 +1,6 @@
-function SmallGameCard({ classId, game, imgSrc, altTxt }) {
+function SmallGameCard({ classId, game, imgSrc, altTxt, path }) {
   return (
-    <a className="card-link" href={game.id ? `/games/${game.id}` : `#`}>
+    <a className="card-link" href={game.id ? `${path}${game.id}` : `#`}>
       <div className="card" id={classId ? classId : 'default'}>
         <img src={game.img ? game.img : imgSrc} alt={game.altTxt ? game.altTxt : altTxt} />
         <p className="game-title-img">{game.nome? game.nome : 'no data'}</p>

8422ffa5a44bab7fb6fa700cffab9b2ecec9bb84
georgegebers <163968415+georgegebers@users.noreply.github.com>
2025-11-24 16:44:42 -0300

    componente botão de genero

---
+++ react/supa-games/src/components/GenreFilter.js
@@ -0,0 +1,22 @@
+import "../styles/pages/games.css"; 
+
+export default function GenreFilter({ genreKeys, gamesByGenre, onScrollToGenre, activeGenre }) {
+    return (
+        <div className="genre-row">
+            <h3>Gêneros</h3>
+
+            <section className="genre-filters">
+                {genreKeys.map((key) => (
+                    <button
+                        key={key}
+                        className={`genre-btn ${activeGenre === key ? "active" : ""}`}
+                        onClick={() => onScrollToGenre(key)}
+                    >
+                        {gamesByGenre[key].name}
+                    </button>
+                ))}
+            </section>
+        </div>
+    );
+}
+
